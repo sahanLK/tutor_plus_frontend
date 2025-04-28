d10772d21ac3255a952014fcb8e4581ce1803484
Sahan <kksahan78@gmail.com>
2025-04-28 16:00:05 +0530

    Job pose details added

--- app/jobs/[jobId]/page.tsx
+++ app/jobs/[jobId]/page.tsx
@@ -5,27 +5,28 @@ import { AxiosError } from "axios";
 import { use, useEffect, useState } from "react";
 
 type PageProps = {
-    params: Promise<{jobId: string}>
+    params: Promise<{ jobId: string }>
 }
 
-export default function JobPostDetails({params}: PageProps) {
-    const {jobId} = use(params);
+export default function JobPostDetails({ params }: PageProps) {
+    const { jobId } = use(params);
     const [post, setPost] = useState({
         id: null,
         title: "",
-        content: ""
+        content: "",
+        applicants: [],
     });
 
     useEffect(() => {
         async function fetchPost() {
             try {
-                const {data} = await api.get(`/posts/${jobId}`);
+                const { data } = await api.get(`/posts/${jobId}`);
                 setPost(data);
             } catch (err) {
                 const error = err as AxiosError;
                 console.log(error);
             }
-            
+
         }
         fetchPost();
     }, []);
@@ -44,6 +45,14 @@ export default function JobPostDetails({params}: PageProps) {
             <h2 className="text-xl text-stone-700 mb-10">Title: {post.title}</h2>
             <div>
                 {post.content}
+            </div>
+            <div className="mt-10">
+                <h1 className="text-xl text-stone-600 mb-5">Applicants</h1>
+                {post.applicants.map(applicant => (
+                    <div key={applicant.id}>
+                        <h2>{applicant.first_name} {applicant.last_name}</h2>
+                    </div>
+                ))}
             </div>
             <button className="mt-20 px-5 py-2 bg-blue-800 text-white rounded" onClick={applyJob}>Apply</button>
         </div>
